dbdf64195d21590f1b13f5bdaa68682170444396
Sung J Byun <sungjbyun@D-10-19-121-48.dhcp4.washington.edu>
2018-05-27 21:56:05 -0700

    initial: leveraging auth-o

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import"
+    ],
+    "rules": {
+        "react/jsx-filename-extension": [1, {"extension": [".js", ".jsx"] }]
+ }
+};
---
+++ client/src/Auth/auth0-variables.js
@@ -0,0 +1,5 @@
+export const AUTH_CONFIG = {
+  domain: 'sjb3.auth0.com',
+  clientId: 'd3xbEu58IigLhGMF463ZMCtio6TifwAl',
+  callbackUrl: 'http://localhost:3000/callback'
+}
